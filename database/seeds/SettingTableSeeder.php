b7de61ccecc1a88832e97c2c95db5917e4ca288d
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-11-01 17:34:12 +0300

    first commit

---
+++ database/seeds/SettingTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data=array(
+            'description'=>".",
+            'short_des'=>"Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.",
+            'photo'=>"image.jpg",
+            'logo'=>'logo.jpg',
+            'address'=>"115 Test Street, Test Country",
+            'email'=>"codeastro.com",
+            'phone'=>"1234567777",
+        );
+        DB::table('settings')->insert($data);
+    }
+}
